28bcdfd95651ea27cb7196f4596395a6d9451436
hossienhunta <hossien021.m@yahoo.com>
2021-02-15 16:28:08 +0330

    Test Api With Axios

---
+++ arcane-mesa/src/components/Hedear.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Hedear body</h1>
+        <button v-on:click="logout('https://arcane-mesa-44871.herokuapp.com/api/v1/users/logout')">خروج</button>
+        <h1>{{ res }}</h1>
+        <div>{{ List }}</div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Hedear',
+    data () {
+        return{
+            res: '',
+            List:{}
+        }
+    },
+    methods: {
+        logout : function(event) {
+        axios.post(event).then(response => {
+                this.res = response.status
+                this.List = response.data
+            }).catch(error => { console.log(error) })
+        }
+    }
+}
+</script>
--- arcane-mesa/src/main.js
+++ arcane-mesa/src/main.js
@@ -3,4 +3,4 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#App')
