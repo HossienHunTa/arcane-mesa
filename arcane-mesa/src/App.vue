a71091ebaecd16877248284ad4ef42f63c545ba6
hossienhunta <hossien021.m@yahoo.com>
2021-02-15 14:02:19 +0330

    Hunta Component

--- arcane-mesa/src/App.vue
+++ arcane-mesa/src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Hunta msg="HunTa" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Hunta from './components/Hunta.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top:20%;
+  background-color:black;
 }
+
 </style>
